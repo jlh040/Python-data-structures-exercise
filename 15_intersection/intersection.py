29a933efe68dcece5da9f48c1232dc098803d86f
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-16 16:43:50 -0700

    implemented intersection()

--- 15_intersection/intersection.py
+++ 15_intersection/intersection.py
@@ -13,3 +13,13 @@ def intersection(l1, l2):
         >>> intersection([1, 2, 3], [4, 5, 6])
         []
     """
+
+    # turn lists into sets
+    set_l1 = set(l1)
+    set_l2 = set(l2)
+
+    # get the intersection
+    intersection_of_lists = set_l1 & set_l2
+
+    #turn back into a list and return
+    return list(intersection_of_lists)
