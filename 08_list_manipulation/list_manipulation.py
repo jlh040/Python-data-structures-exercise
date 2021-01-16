667aadc06bc176eff7163a6615d737c0f1e10f8e
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-16 15:21:58 -0700

    implemented list_manipulation()

--- 08_list_manipulation/list_manipulation.py
+++ 08_list_manipulation/list_manipulation.py
@@ -40,3 +40,23 @@ def list_manipulation(lst, command, location, value=None):
         >>> list_manipulation(lst, 'add', 'dunno') is None
         True
     """
+    commands = ['add', 'remove']
+    locations = ['beginning', 'end']
+
+    if command not in commands:
+        return None
+    elif location not in locations:
+        return None
+    else:
+        if command == 'add' and location == 'beginning':
+            lst.insert(0, value)
+            return lst
+        elif command == 'add' and location == 'end':
+            lst.append(value)
+            return lst
+        elif command == 'remove' and location == 'beginning':
+            return lst.pop(0)
+        else:
+            return lst.pop()
+
+
