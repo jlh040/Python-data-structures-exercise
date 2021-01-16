1aaaca515a7f0263c7786ae0a291edc03a9757f9
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-16 16:25:12 -0700

    implemented flip_case()

--- 11_flip_case/flip_case.py
+++ 11_flip_case/flip_case.py
@@ -11,3 +11,19 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    # get uppercase and lowercase of to_swap:
+    to_swap_both = [to_swap.upper(), to_swap.lower()]
+
+    # turn phrase into list
+    phrase_list = list(phrase)
+
+    # iterate and swap casing
+    for idx in range(len(phrase_list)):
+        if phrase_list[idx] in to_swap_both and phrase_list[idx].isupper():
+            phrase_list[idx] = phrase_list[idx].lower()
+        elif phrase_list[idx] in to_swap_both and phrase_list[idx].islower():
+            phrase_list[idx] = phrase_list[idx].upper()
+    
+    # return a concatenated version of the list
+    return ''.join(phrase_list)
+        
