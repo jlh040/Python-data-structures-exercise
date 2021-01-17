25ffc66f2eb0b725d8da679e996af9ca59f5d944
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-16 17:50:12 -0700

    implemented mode()

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,20 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    # make a dictionary of frequencies
+    freq_dict = {number: nums.count(number) for number in nums}
+
+    # set a default mode and the number of times it occurs
+    largest = freq_dict[list(freq_dict.keys())[0]]
+    mode_of_list = list(freq_dict.keys())[0]
+
+    # if one of the frequences is larger then the current largest frequency
+    # set the mode to the corresponding number
+    for k in freq_dict.keys():
+        if freq_dict[k] > largest:
+            mode_of_list = k
+            largest = freq_dict[k]
+    
+    # return the mode
+    return mode_of_list
+    
