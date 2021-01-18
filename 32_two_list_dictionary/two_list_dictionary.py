11a2e44b0a59e2feef924e539ed564d3f83a727a
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-17 21:28:23 -0700

    implemented two_list_dictionary()

--- 32_two_list_dictionary/two_list_dictionary.py
+++ 32_two_list_dictionary/two_list_dictionary.py
@@ -1,6 +1,6 @@
 def two_list_dictionary(keys, values):
     """Given keys and values, make dictionary of those.
-    
+
         >>> two_list_dictionary(['x', 'y', 'z'], [9, 8, 7])
         {'x': 9, 'y': 8, 'z': 7}
         
@@ -14,4 +14,15 @@ def two_list_dictionary(keys, values):
 
         >>> two_list_dictionary(['a', 'b', 'c'], [1, 2, 3, 4])
         {'a': 1, 'b': 2, 'c': 3}
-   """
+    """
+    if len(keys) == len(values):
+        return {keys[i]:values[i] for i in range(len(keys))}
+    elif len(keys) > len(values):
+        values[len(keys) + 1:] = [None]
+        our_dict = {keys[i]:values[i] for i in range(len(keys))}
+        return our_dict
+    else:
+        return {keys[i]:values[i] for i in range(len(keys))}
+
+
+       
